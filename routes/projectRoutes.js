b88d4550cb27f113520b36fc47ac0130e2850734
Pawan <pawanmishra196@gmail.com>
2026-04-20 17:04:27 +0530

    fix: delete tasks of a project if project deletes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,4 +1,5 @@
 const Project = require("../models/Project");
+const Task = require("../models/Task");
 const projectRouter = require("express").Router();
 const auth = require("../middleware/auth");
 
@@ -45,13 +46,21 @@ projectRouter.get("/:id", auth, async (req, res) => {
 // DELETE project
 projectRouter.delete("/:id", auth, async (req, res) => {
   try {
-    const deletedProject = await Project.findByIdAndDelete(req.params.id);
+    const project = await Project.findById(req.params.id);
 
-    if (!deletedProject) {
+    if (!project) {
       return res.status(404).json({ message: "Project not found" });
     }
 
-    res.json({ message: "Project deleted successfully", deletedProject });
+    //delete all tasks of this project
+    await Task.deleteMany({ project: req.params.id });
+
+    const deletedProject = await Project.findByIdAndDelete(req.params.id);
+
+    res.json({
+      message: "Project and related tasks deleted successfully",
+      deletedProject,
+    });
   } catch (error) {
     console.log("Delete project error", error);
     res.status(500).json({ message: "Failed to delete project" });
